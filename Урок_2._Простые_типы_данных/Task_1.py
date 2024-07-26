661939dc5243c9dc25f19e418006e1bdf1c88335
Sarmat <Spirator@Yandex.ru>
2024-07-26 13:44:37 +0700

    Решена первая задача

--- Урок_2._Простые_типы_данных/Task_1.py
+++ Урок_2._Простые_типы_данных/Task_1.py
@@ -4,3 +4,23 @@
 Создайте несколько переменый разных типов.
 Проверте к какому типу относяться созданные переменные.
 """
+name = 'John'
+print(type(name))
+
+age = 30
+print(type(age))
+
+height = 1.85
+print(type(height))
+
+is_student = False
+print(type(is_student))
+
+hobbies = ['reading', 'swimming']
+print(type(hobbies))
+
+address = ('New York', 'USA')
+print(type(address))
+
+personal_info = {'name': 'John', 'age': 30, 'height': 1.85}
+print(type(personal_info))
