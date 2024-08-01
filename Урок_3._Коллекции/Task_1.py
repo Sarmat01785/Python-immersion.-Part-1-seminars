2fe8f0012024e5371236e75e0dafd761a3bb04ee
Sarmat <Spirator@Yandex.ru>
2024-08-01 10:19:25 +0700

    End

---
+++ Урок_3._Коллекции/Task_1.py
@@ -0,0 +1,33 @@
+'''
+Задача 1
+
+Вручную создайте список с целыми числами,которые повторяються.Получите новый список,
+который содержит уникальные (без повтора ) элементы исходного списка.
+
+Подготовте два решения, короткое и длинное, которое не использует другие коллекции помимо списков.
+'''
+
+# Вариант короткое решение
+original_list = [1, 2, 3, 2, 4, 5, 5, 6, 1]
+
+unique_list = list(set(original_list))
+
+print(original_list)
+print(unique_list)
+
+
+
+# Вариант длинное решение
+
+original_list = [1, 2, 3, 2, 4, 5, 5, 6, 1]
+
+unique_list = []
+
+for item in original_list:
+    if item not in unique_list:
+        unique_list.append(item)
+
+print(original_list)
+print(unique_list)
+
+
