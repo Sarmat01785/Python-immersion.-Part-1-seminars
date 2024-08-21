628863941a57f67c437881d26340aa1d81e554d7
Sarmat <Spirator@Yandex.ru>
2024-08-21 18:02:24 +0700

    end

---
+++ Урок_6._Модули/Task_1.py
@@ -0,0 +1,16 @@
+'''
+Задание №1
+Вспомните какие модули вы уже проходили на курсе.
+Создайте файл, в котором вы импортируете встроенные в
+модуль функции под псевдонимами. (3-7 строк импорта).
+'''
+import random as rnd
+import math as mt
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import scipy as sp
+from decimal import Decimal as D
+from sys import argv as ARGV
+from fractions import Fraction as F
