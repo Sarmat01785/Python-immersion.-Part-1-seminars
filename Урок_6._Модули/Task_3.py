9667f21a35923e8e4c12252a9943642c1679a6b3
Sarmat <Spirator@Yandex.ru>
2024-08-21 22:09:16 +0700

    Задача 3 решена

---
+++ Урок_6._Модули/Task_3.py
@@ -0,0 +1,64 @@
+"""
+Задание №3
+Улучшаем задачу 2.
+Добавьте возможность запуска функции "угадайки" из
+модуля в командной строке терминала.
+Строка должна принимать от 1 до 3 аргументов: параметры
+вызова функции.
+Для преобразования строковых аргументов командной
+строки в числовые параметры используйте генераторное
+выражение.
+"""
+
+# guess_number.py
+import random
+import sys
+
+
+def guess_the_number(lower_bound=1, upper_bound=100, attempts=5):
+    """
+    Игра "Угадай число". Загадывается число в заданном диапазоне,
+    и пользователь пытается его угадать за ограниченное количество попыток.
+
+    :param lower_bound: Нижняя граница диапазона чисел.
+    :param upper_bound: Верхняя граница диапазона чисел.
+    :param attempts: Количество попыток для угадывания числа.
+    :return: True, если число угадано, иначе False.
+    """
+    secret_number = random.randint(lower_bound, upper_bound)
+    print(f"Угадайте число между {lower_bound} и {upper_bound} за {attempts} попыток.")
+
+    for attempt in range(attempts):
+        try:
+            guess = int(input(f"Попытка {attempt + 1}. Введите ваше число: "))
+            if guess == secret_number:
+                print("Поздравляем! Вы угадали число!")
+                return True
+            elif guess < secret_number:
+                print("Больше.")
+            else:
+                print("Меньше.")
+        except ValueError:
+            print("Пожалуйста, введите целое число.")
+
+    print(f"Вы исчерпали все попытки. Загаданное число было: {secret_number}")
+    return False
+
+
+if __name__ == "__main__":
+    # Проверяем количество переданных аргументов
+    if 1 <= len(sys.argv[1:]) <= 3:
+        # Используем генераторное выражение для преобразования аргументов командной строки в числа
+        params = [int(arg) for arg in sys.argv[1:]]
+
+        # Распаковываем параметры и вызываем функцию
+        guess_the_number(*params)
+    else:
+        print("Usage: python guess_number.py <lower_bound> <upper_bound> <attempts>")
+        print(
+            "lower_bound - Нижняя граница диапазона (необязательный параметр, по умолчанию 1)"
+        )
+        print(
+            "upper_bound - Верхняя граница диапазона (необязательный параметр, по умолчанию 100)"
+        )
+        print("attempts - Количество попыток (необязательный параметр, по умолчанию 5)")
